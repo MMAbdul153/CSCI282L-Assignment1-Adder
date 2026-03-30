8fc425c50799fd724ed9bcd00881fc4f1b74c1e4
Musa Abdulrafiu <musaabdulrafiu20@gmail.com>
2026-03-29 23:45:16 -0500

    Week 3 Cobra implementation

--- runtime/start.rs
+++ runtime/start.rs
@@ -3,8 +3,13 @@ extern "C" {
     #[link_name = "\x01our_code_starts_here"]
     fn our_code_starts_here() -> i64;
 }
+#[no_mangle]
+pub extern "C" fn snek_error(errcode: i64) {
+    println!("Error {}", errcode);
+    std::process::exit(1);
+}
 
 fn main() {
     let result: i64 = unsafe { our_code_starts_here() };
     println!("{}", result);
-}
+} 
--- src/main.rs
+++ src/main.rs
@@ -18,22 +18,33 @@ enum Op2 {
     Plus,
     Minus,
     Times,
+    Equal,
+    Greater,
+    GreaterEqual,
+    Less,
+    LessEqual,
 }
 
 #[derive(Debug)]
 enum Expr {
     Number(i32),
+    Boolean(bool),
     Id(String),
     Let(Vec<(String, Expr)>, Box<Expr>),
     UnOp(Op1, Box<Expr>),
     BinOp(Op2, Box<Expr>, Box<Expr>),
+    If(Box<Expr>, Box<Expr>, Box<Expr>),
 }
 
-/// Converts S-expressions (like "(add1 5)") into our internal Expr AST.
+// ================= PARSER =================
+
 fn parse_expr(s: &Sexp) -> Expr {
     match s {
 
         Sexp::Atom(I(n)) => Expr::Number(i32::try_from(*n).unwrap()),
+
+        Sexp::Atom(S(s)) if s == "true" => Expr::Boolean(true),
+        Sexp::Atom(S(s)) if s == "false" => Expr::Boolean(false),
 
         Sexp::Atom(S(name)) => Expr::Id(name.to_string()),
 
@@ -46,19 +57,35 @@ fn parse_expr(s: &Sexp) -> Expr {
                 Expr::UnOp(Op1::Sub1, Box::new(parse_expr(e))),
 
             [Sexp::Atom(S(op)), e1, e2] if op == "+" =>
-                Expr::BinOp(Op2::Plus,
-                    Box::new(parse_expr(e1)),
-                    Box::new(parse_expr(e2))),
+                Expr::BinOp(Op2::Plus, Box::new(parse_expr(e1)), Box::new(parse_expr(e2))),
 
             [Sexp::Atom(S(op)), e1, e2] if op == "-" =>
-                Expr::BinOp(Op2::Minus,
-                    Box::new(parse_expr(e1)),
-                    Box::new(parse_expr(e2))),
+                Expr::BinOp(Op2::Minus, Box::new(parse_expr(e1)), Box::new(parse_expr(e2))),
 
             [Sexp::Atom(S(op)), e1, e2] if op == "*" =>
-                Expr::BinOp(Op2::Times,
-                    Box::new(parse_expr(e1)),
-                    Box::new(parse_expr(e2))),
+                Expr::BinOp(Op2::Times, Box::new(parse_expr(e1)), Box::new(parse_expr(e2))),
+
+            [Sexp::Atom(S(op)), e1, e2] if op == "=" =>
+                Expr::BinOp(Op2::Equal, Box::new(parse_expr(e1)), Box::new(parse_expr(e2))),
+
+            [Sexp::Atom(S(op)), e1, e2] if op == ">" =>
+                Expr::BinOp(Op2::Greater, Box::new(parse_expr(e1)), Box::new(parse_expr(e2))),
+
+            [Sexp::Atom(S(op)), e1, e2] if op == ">=" =>
+                Expr::BinOp(Op2::GreaterEqual, Box::new(parse_expr(e1)), Box::new(parse_expr(e2))),
+
+            [Sexp::Atom(S(op)), e1, e2] if op == "<" =>
+                Expr::BinOp(Op2::Less, Box::new(parse_expr(e1)), Box::new(parse_expr(e2))),
+
+            [Sexp::Atom(S(op)), e1, e2] if op == "<=" =>
+                Expr::BinOp(Op2::LessEqual, Box::new(parse_expr(e1)), Box::new(parse_expr(e2))),
+
+            [Sexp::Atom(S(op)), cond, thn, els] if op == "if" =>
+                Expr::If(
+                    Box::new(parse_expr(cond)),
+                    Box::new(parse_expr(thn)),
+                    Box::new(parse_expr(els)),
+                ),
 
             [Sexp::Atom(S(op)), Sexp::List(bindings), body] if op == "let" =>
                 Expr::Let(parse_bind(bindings), Box::new(parse_expr(body))),
@@ -71,27 +98,23 @@ fn parse_expr(s: &Sexp) -> Expr {
 }
 
 fn parse_bind(bindings: &Vec<Sexp>) -> Vec<(String, Expr)> {
-
     let mut result = Vec::new();
 
     for bind in bindings {
-
         match bind {
-
             Sexp::List(vec) => match &vec[..] {
-
                 [Sexp::Atom(S(name)), expr] =>
                     result.push((name.to_string(), parse_expr(expr))),
-
                 _ => panic!("Invalid binding"),
             },
-
             _ => panic!("Invalid binding"),
         }
     }
 
     result
 }
+
+// ================= COMPILER =================
 
 fn compile_to_instrs(
     e: &Expr,
@@ -102,16 +125,15 @@ fn compile_to_instrs(
     match e {
 
         Expr::Number(n) =>
-            format!("mov rax, {}", n),
+            format!("mov rax, {}", n << 1),
+
+        Expr::Boolean(true) => "mov rax, 3".to_string(),
+        Expr::Boolean(false) => "mov rax, 1".to_string(),
 
         Expr::Id(name) => {
             match env.get(name) {
-
-                Some(offset) =>
-                    format!("mov rax, [rsp{}]", offset),
-
-                None =>
-                    panic!("Unbound variable identifier {}", name),
+                Some(offset) => format!("mov rax, [rsp{}]", offset),
+                None => panic!("Unbound variable identifier {}", name),
             }
         }
 
@@ -119,44 +141,106 @@ fn compile_to_instrs(
 
             let sub = compile_to_instrs(expr, si, env);
 
+            let check = "
+test rax, 1
+jnz throw_error
+";
+
             match op {
-
                 Op1::Add1 =>
-                    format!("{}\nadd rax, 1", sub),
-
+                    format!("{}\n{}\nadd rax, 2", sub, check),
                 Op1::Sub1 =>
-                    format!("{}\nsub rax, 1", sub),
+                    format!("{}\n{}\nsub rax, 2", sub, check),
             }
         }
 
         Expr::BinOp(op, e1, e2) => {
 
             let left = compile_to_instrs(e1, si, env);
+            let save = format!("mov [rsp{}], rax", -8 * si);
+            let right = compile_to_instrs(e2, si + 1, env);
 
-            let save =
-                format!("mov [rsp{}], rax", -8 * si);
-
-            let right =
-                compile_to_instrs(e2, si + 1, env);
+            let check = "
+test rax, 1
+jnz throw_error
+";
 
             let instr = match op {
 
                 Op2::Plus =>
-                    format!("add rax, [rsp{}]", -8 * si),
+                    format!("{}\nadd rax, [rsp{}]", check, -8 * si),
 
                 Op2::Minus =>
                     format!(
-                        "mov rbx, [rsp{}]\nsub rbx, rax\nmov rax, rbx",
-                        -8 * si
+                        "{}\nmov rbx, [rsp{}]\nsub rbx, rax\nmov rax, rbx",
+                        check, -8 * si
                     ),
 
                 Op2::Times =>
-                    format!("imul rax, [rsp{}]", -8 * si),
+                    format!(
+                        "{}\nsar rax, 1\nimul rax, [rsp{}]",
+                        check, -8 * si
+                    ),
+
+                Op2::Equal =>
+                    format!(
+                        "cmp rax, [rsp{}]\nmov rax, 3\nje done{}\nmov rax, 1\ndone{}:",
+                        -8 * si, si, si
+                    ),
+
+                Op2::Greater =>
+                    format!(
+                        "cmp [rsp{}], rax\nmov rax, 3\njg done{}\nmov rax, 1\ndone{}:",
+                        -8 * si, si, si
+                    ),
+
+                Op2::GreaterEqual =>
+                    format!(
+                        "cmp [rsp{}], rax\nmov rax, 3\njge done{}\nmov rax, 1\ndone{}:",
+                        -8 * si, si, si
+                    ),
+
+                Op2::Less =>
+                    format!(
+                        "cmp [rsp{}], rax\nmov rax, 3\njl done{}\nmov rax, 1\ndone{}:",
+                        -8 * si, si, si
+                    ),
+
+                Op2::LessEqual =>
+                    format!(
+                        "cmp [rsp{}], rax\nmov rax, 3\njle done{}\nmov rax, 1\ndone{}:",
+                        -8 * si, si, si
+                    ),
             };
 
+            format!("{}\n{}\n{}\n{}", left, save, right, instr)
+        }
+
+        Expr::If(cond, thn, els) => {
+
+            let cond_code = compile_to_instrs(cond, si, env);
+            let thn_code = compile_to_instrs(thn, si, env);
+            let els_code = compile_to_instrs(els, si, env);
+
+            let else_label = format!("else_{}", si);
+            let end_label = format!("ifend_{}", si);
+
             format!(
-                "{}\n{}\n{}\n{}",
-                left, save, right, instr
+                "{}
+cmp rax, 1
+je {}
+{}
+jmp {}
+{}:
+{}
+{}:",
+                cond_code,
+                else_label,
+                thn_code,
+                end_label,
+                else_label,
+                els_code,
+                end_label
             )
         }
 
@@ -173,15 +257,9 @@ fn compile_to_instrs(
                 }
 
                 code += &compile_to_instrs(expr, new_si, &new_env);
+                code += &format!("\nmov [rsp{}], rax\n", -8 * new_si);
 
-                code += &format!(
-                    "\nmov [rsp{}], rax\n",
-                    -8 * new_si
-                );
-
-                new_env =
-                    new_env.update(name.clone(), -8 * new_si);
-
+                new_env = new_env.update(name.clone(), -8 * new_si);
                 new_si += 1;
             }
 
@@ -195,44 +273,46 @@ fn compile_to_instrs(
 fn compile(e: &Expr) -> String {
 
     let env = HashMap::new();
-
     let body = compile_to_instrs(e, 2, &env);
 
     format!(
         "
 section .text
+extern snek_error
 global our_code_starts_here
 
 our_code_starts_here:
 {}
 ret
+
+throw_error:
+mov rdi, 1
+call snek_error
 ",
         body
     )
 }
 
+// ================= MAIN =================
+
 fn main() -> io::Result<()> {
+
     let args: Vec<String> = env::args().collect();
 
-    // Basic check: Ensure we have enough arguments
     if args.len() < 3 {
-        panic!("Usage: cargo run -- <input_file> <output_file>");
+        panic!("Usage: cargo run -- <input> <output>");
     }
 
-    let in_name = &args[1];
-    let out_name = &args[2];
-
-    let mut in_file = File::open(in_name)?;
+    let mut in_file = File::open(&args[1])?;
     let mut contents = String::new();
     in_file.read_to_string(&mut contents)?;
 
     let parsed = parse(&contents).unwrap();
-
     let expr = parse_expr(&parsed);
 
     let result = compile(&expr);
 
-    let mut out_file = File::create(out_name)?;
+    let mut out_file = File::create(&args[2])?;
     out_file.write_all(result.as_bytes())?;
 
     Ok(())
